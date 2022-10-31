2f198ea7c854f22927222cf3315b1952974500eb
Vassilis Salis <vasalis@microsoft.com>
2022-10-31 11:35:01 +0200

    Add project files.

---
+++ MyScaleTest.cs
@@ -0,0 +1,66 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Xml.Linq;
+
+namespace AzScaleFun
+{
+    public static class MyScaleTest
+    {
+        [FunctionName("MyScaleTest")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            var lStateTime = DateTime.UtcNow.Ticks;
+            var lIterations = 1000;
+
+            for (int i = 0; i < lIterations; i++)
+            {
+                FindPrimeNumber(1000);
+            }
+
+            var lEndTime = DateTime.UtcNow.Ticks;
+
+            var lTicks = lEndTime - lStateTime;
+
+            string responseMessage = $"Ticks taken: {lTicks}";
+
+            log.LogMetric("TicksTaken", lTicks);
+
+            return new OkObjectResult(responseMessage);
+        }
+
+        private static long FindPrimeNumber(int n)
+        {
+            int count = 0;
+            long a = 2;
+            while (count < n)
+            {
+                long b = 2;
+                int prime = 1;// to check if found a prime
+                while (b * b <= a)
+                {
+                    if (a % b == 0)
+                    {
+                        prime = 0;
+                        break;
+                    }
+                    b++;
+                }
+                if (prime > 0)
+                {
+                    count++;
+                }
+                a++;
+            }
+            return (--a);
+        }
+    }
+}
